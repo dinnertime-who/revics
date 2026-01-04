293baf92e9f7a9331a3cd4b6d9c3acb57423803d
dinnertime-who <dinnertime.dev@gmail.com>
2026-01-05 05:37:53 +0900

    Add font weight examples to RouteComponent in _platform-layout

--- src/routes/_platform-layout/index.tsx
+++ src/routes/_platform-layout/index.tsx
@@ -6,5 +6,14 @@ export const Route = createFileRoute("/_platform-layout/")({
 });
 
 function RouteComponent() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="font-light">Light Font</div>
+      <div className="font-normal">Normal Font</div>
+      <div className="font-medium">Medium Font</div>
+      <div className="font-semibold">Semibold Font</div>
+      <div className="font-bold">Bold Font</div>
+      <div className="font-extrabold">Extrabold Font</div>
+    </div>
+  );
 }
